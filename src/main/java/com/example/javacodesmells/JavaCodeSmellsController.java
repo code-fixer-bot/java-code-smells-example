f89ca803ebc6bd7228c659b967a6e808683c9312
vilaca <jvilaca@gmail.com>
2019-07-23 23:21:21 +0100

    added ctrller

--- src/main/java/com/example/javacodesmells/JavaCodeSmellsController.java
+++ src/main/java/com/example/javacodesmells/JavaCodeSmellsController.java
@@ -3,7 +3,7 @@ package com.example.javacodesmells;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 public class JavaCodeSmellsController {
-    @RequestMapping("/greet") // Noncompliant
+    @RequestMapping("/greet") // Noncompliant (source: sonarqube)
     public String greet(String greetee) {
         return "Howdy!";
     }
