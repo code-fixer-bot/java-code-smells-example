01d8b7802fc81fba72d224022db2a923df6deb33
vilaca <jvilaca@gmail.com>
2019-07-23 23:13:45 +0100

    first commit

---
+++ src/main/java/com/example/javacodesmells/JavaCodeSmellsApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacodesmells;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeSmellsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodeSmellsApplication.class, args);
+	}
+
+}
