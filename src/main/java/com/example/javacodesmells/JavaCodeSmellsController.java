b74b9c2a8d57545d081b8d714e039b35ff728774
vilaca <jvilaca@gmail.com>
2019-07-24 00:19:22 +0100

    annotations

--- src/main/java/com/example/javacodesmells/JavaCodeSmellsController.java
+++ src/main/java/com/example/javacodesmells/JavaCodeSmellsController.java
@@ -1,7 +1,11 @@
 package com.example.javacodesmells;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@ResponseBody
 public class JavaCodeSmellsController {
     @RequestMapping("/greet") // Noncompliant (source: sonarqube)
     public String greet(String greetee) {
