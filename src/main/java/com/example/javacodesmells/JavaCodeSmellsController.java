d8b967aded1623b310b5b923a5fa7d5dfab64d44
vilaca <jvilaca@gmail.com>
2019-07-24 00:59:01 +0100

    field

--- src/main/java/com/example/javacodesmells/JavaCodeSmellsController.java
+++ src/main/java/com/example/javacodesmells/JavaCodeSmellsController.java
@@ -4,13 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-// controller comment
 @Controller("value")
-// responsebody comment
 @ResponseBody
 public class JavaCodeSmellsController {
+    
+    final private static String GREETING = "Howdy!";
+    
     @RequestMapping("/greet") // Noncompliant (source: sonarqube)
     public String greet(String greetee) {
-        return "Howdy!";
+        return GREETING;
     }
 }
